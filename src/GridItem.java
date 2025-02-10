390137b87aa80b5b28eab92c7288b97d839d8f6a
Joe Davis <Jdavis2@softwareinstitute.com>
2025-02-10 10:01:59 +0000

    Added object implementation, bomb implementation, status initialisation

--- src/GridItem.java
+++ src/GridItem.java
@@ -1,18 +1,27 @@
 public class GridItem {
     private int status=0;
-    private boolean revealed = false;
+    private boolean revealed = true;
     private boolean isBomb = false;
 
     public int getStatus(){
-        if(revealed){
+        if(this.isBomb){
+            return 99;
+        } else if(this.revealed){
             return this.status;
         }
         else{
             return 10;
         }
     }
+    public void updateStatus(){
+        this.status = this.status+1;
+    }
+    public boolean getBomb(){
+        return this.isBomb;
+    }
     public GridItem(float decision){
-        if(decision<10){
+        //System.out.println(decision);
+        if(decision==1){
             isBomb=true;
         }
     }
--- src/Gridtiles.java
+++ src/Gridtiles.java
@@ -1,16 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.lang.reflect.Array;
 import java.util.ArrayList;
-
+import java.util.Random;
 public class Gridtiles {
     private static final int rows = 15;
     private static final int columns = 15;
     private int rowit =1;
     private int colit=0;
-    private ArrayList<ArrayList<String>> gridmap = new ArrayList<ArrayList<String>>();
+    private ArrayList<ArrayList<GridItem>> gridmap = new ArrayList<ArrayList<GridItem>>();
 
 
-
+    public ArrayList<ArrayList<GridItem>> getGridmap(){
+        return this.gridmap;
+    }
     public int getRows(){
         return this.rows;
     }
@@ -19,9 +22,11 @@ public class Gridtiles {
     }
     public void initialisemap(){
         for(int RC=0;RC<this.rows;RC++){
-            ArrayList<String> temp = new ArrayList<String>();
+            ArrayList<GridItem> temp = new ArrayList<GridItem>();
             for(int CC=0;CC<this.rows;CC++){
-                temp.add("X");
+                Random rn = new Random();
+                int decider = rn.nextInt(10);
+                temp.add(new GridItem(decider));
             }
         this.gridmap.add(temp);
 
@@ -30,9 +35,21 @@ public class Gridtiles {
     }
     public void DisplayMap(){
         this.rowit=1;
-        System.out.println("   1  2  3  4  5  6  7  8  9  10 11 12 13 14 15");
-        for(ArrayList<String> RL:this.gridmap){
-            System.out.print(rowit);
+        System.out.println("------------------------------------------------");
+        System.out.println("     1  2  3  4  5  6  7  8  9  10 11 12 13 14 15");
+        System.out.println("------------------------------------------------");
+        for(ArrayList<GridItem> RL:this.gridmap){
+
+            if(String.valueOf(rowit).length()<2){
+                System.out.print(" ");
+                System.out.print(rowit);
+                System.out.print("|");
+            }else{
+                System.out.print(rowit);
+                System.out.print("|");
+            }
+            System.out.print("  ");
+            /*  Legacy formatting. kept incase of reform
             switch(String.valueOf(rowit).length()){
                 case 1:
                     System.out.print("  ");
@@ -40,11 +57,11 @@ public class Gridtiles {
                 case 2:
                     System.out.print(" ");
                     break;
-            }
+            }*/
             rowit=rowit+1;
-            for(String CL:RL){
-                System.out.print(CL);
-                switch(CL.length()){
+            for(GridItem CL:RL){
+                System.out.print(CL.getStatus());
+                switch(String.valueOf(CL.getStatus()).length()){
                     case 1:
                         System.out.print("  ");
                         break;
@@ -55,13 +72,81 @@ public class Gridtiles {
             }
             System.out.print("\n");
         }
+        System.out.println("------------------------------------------------");
     }
+    public void initialiseStatus() {
+        for(int RC=0;RC<this.rows;RC++){
 
 
+            for(int CC=0;CC<this.rows;CC++){
+                boolean topcheck = true;
+                boolean leftcheck = true;
+                boolean botcheck = true;
+                boolean rightcheck = true;
+                if(RC==0){
+                    topcheck = false;
+                }
+                if(RC==(this.rows-1)){
+                    botcheck = false;
+                }
+                if(CC==0){
+                    leftcheck=false;
+                }
+                if(CC==(this.columns-1)){
+                    rightcheck=false;
+                }
+                if(topcheck){
+                    if(leftcheck){
+                        if(this.gridmap.get(RC-1).get(CC-1).getBomb()){
+                            this.gridmap.get(RC).get(CC).updateStatus();
+                        }
+                    }
+                    if(this.gridmap.get(RC-1).get(CC).getBomb()){
+                        this.gridmap.get(RC).get(CC).updateStatus();
+                    }
+                    if(rightcheck){
+                        if(this.gridmap.get(RC-1).get(CC+1).getBomb()){
+                            this.gridmap.get(RC).get(CC).updateStatus();
+                        }
+                    }
+                }
+                if(botcheck){
+                    if(leftcheck){
+                        if(this.gridmap.get(RC+1).get(CC-1).getBomb()){
+                            this.gridmap.get(RC).get(CC).updateStatus();
+                        }
+                    }
+                    if(this.gridmap.get(RC+1).get(CC).getBomb()){
+                        this.gridmap.get(RC).get(CC).updateStatus();
+                    }
+                    if(rightcheck){
+                        if(this.gridmap.get(RC+1).get(CC+1).getBomb()){
+                            this.gridmap.get(RC).get(CC).updateStatus();
+                        }
+                    }
+                }
+                if(leftcheck){
+                    if(this.gridmap.get(RC).get(CC-1).getBomb()){
+                        this.gridmap.get(RC).get(CC).updateStatus();
+                    }
+                }
+                if(rightcheck){
+                    if(this.gridmap.get(RC).get(CC+1).getBomb()){
+                        this.gridmap.get(RC).get(CC).updateStatus();
+                    }
+                }
+
+                //this.gridmap.get(RC).get(CC).updateStatus();
+                //System.out.println(this.gridmap.get(RC).get(CC).getStatus());
+            }
+        }
+    }
 
     public static void main(String[] args) {
         Gridtiles Gameenv = new Gridtiles();
         Gameenv.initialisemap();
+        Gameenv.DisplayMap();
+        Gameenv.initialiseStatus();
         Gameenv.DisplayMap();
     }
 
